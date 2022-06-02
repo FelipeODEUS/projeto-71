b36b004c6362f543e9047b2f25bf9f133e2cd41f
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2022-06-02 17:44:33 -0300

    projeto concluido

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  const firebaseConfig = {
     apiKey: "AIzaSyDNOhwPSat4nh21W5cMWEpcHYMXP1lH_Dg",
     authDomain: "aluguel-de-bike.firebaseapp.com",
     projectId: "aluguel-de-bike",
@@ -11,7 +10,7 @@ var firebaseConfig = {
     appId: "1:127010230342:web:f689bae1e4a99090f37a47"
   };
   
-};
+
 
 firebase.initializeApp(firebaseConfig);
 
